a4a1c685ac075afcba1cf2baa206cc5cd1583305
SidneyRamphalile <72583663+SidneyRamphalile@users.noreply.github.com>
2026-02-25 09:54:24 +0200

    Updated Latest Project

--- app/components/ProjectsSection.jsx
+++ app/components/ProjectsSection.jsx
@@ -7,9 +7,20 @@ import { animate, motion, useInView } from "framer-motion";
 const projectsData = [
   {
     id: 1,
+    title: "Accounting Insights",
+    description:
+      "This is a comprehensive Accounting Insights website for an accounting company.",
+    image: "/images/projects/accounting-insights.png",
+    tag: ["All", "Web"],
+    gitUrl: "https://github.com/SidneyRamphalile/accounting-insights",
+    target: "_blank",
+    previewUrl: "https://storied-snickerdoodle-46ab37.netlify.app/",
+  },
+  {
+    id: 2,
     title: "Steel Pulse",
     description:
-      "This is a front-end landing page for a fictional Gym App called Steel Pulse, built using Svelte",
+      "This is a front-end landing page for a fictional Gym App called Steel Pulse, built using Svelte.",
     image: "/images/projects/steel-pulse.png",
     tag: ["All", "Web"],
     gitUrl:
@@ -18,7 +29,7 @@ const projectsData = [
     previewUrl: "https://melodious-mermaid-f0a544.netlify.app/",
   },
   {
-    id: 2,
+    id: 3,
     title: "Kreativ Design & Print",
     description:
       "This is a website of Kreativ Design & Print, a company in Bloemfontein, built using ReactJS.",
@@ -29,7 +40,7 @@ const projectsData = [
     previewUrl: "https://polite-quokka-0e16a4.netlify.app/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Namanyane Primary School",
     description:
       "This is a website of Namanyane Primary School, a school in my hometown, built using ReactJS.",
@@ -40,7 +51,7 @@ const projectsData = [
     previewUrl: "https://stunning-halva-4a5ca5.netlify.app/",
   },
   {
-    id: 4,
+    id: 5,
     title: "Advanced MERN Authentication",
     description:
       "This is a MERN Authentication where users can create an account, etc. It has a forgot password feature.",
@@ -51,7 +62,7 @@ const projectsData = [
     previewUrl: "https://advanced-mern-auth-agj6.onrender.com/login",
   },
   {
-    id: 5,
+    id: 6,
     title: "Mini Store",
     description:
       "This is a mini store where a user can create, read, update, and delete products.",
@@ -62,7 +73,7 @@ const projectsData = [
     previewUrl: "https://mini-store-2lla.onrender.com/",
   },
   {
-    id: 6,
+    id: 7,
     title: "Edusity",
     description:
       "This is a website of a fictional university called Edusity, built using ReactJS.",
@@ -73,7 +84,7 @@ const projectsData = [
     previewUrl: "https://adorable-squirrel-9d1076.netlify.app/",
   },
   {
-    id: 7,
+    id: 8,
     title: "Book My Show Clone",
     description:
       "This is a clone of the Book My Show website, where one can buy or rent movies.",
@@ -85,7 +96,7 @@ const projectsData = [
     previewUrl: "https://book-my-show-clone-application.vercel.app/",
   },
   {
-    id: 8,
+    id: 9,
     title: "Zomato Landing Page",
     description: "This is a Front-End landing page of Zomato clone.",
     image: "/images/projects/zomato-landing-page.png",
@@ -95,7 +106,7 @@ const projectsData = [
     previewUrl: "https://zomato-landing-page-clone-za.netlify.app/",
   },
   {
-    id: 9,
+    id: 10,
     title: "Eduford",
     description:
       "This is a Front-End of a fictitious university called Eduford.",
@@ -106,7 +117,7 @@ const projectsData = [
     previewUrl: "https://eduford-fictitious-university.netlify.app/",
   },
   {
-    id: 10,
+    id: 11,
     title: "Business Website",
     description: "This is a Front-End of a fictitious business website.",
     image: "/images/projects/business-website.png",
@@ -116,7 +127,7 @@ const projectsData = [
     previewUrl: "https://sid-business-website.netlify.app/",
   },
   {
-    id: 11,
+    id: 12,
     title: "Disney+ Hotstar Clone",
     description: "This is a Front-End of the Disney+ Hotstar clone.",
     image: "/images/projects/hotstar.png",
@@ -126,7 +137,7 @@ const projectsData = [
     previewUrl: "https://disney-hot-naledi-clone.netlify.app/",
   },
   {
-    id: 12,
+    id: 13,
     title: "Vegetarian Taco Shop",
     description: "This is a Front-End of a fictitious Vegetarian Taco Shop.",
     image: "/images/projects/vts.png",
@@ -137,7 +148,7 @@ const projectsData = [
     previewUrl: "https://imaginative-sprite-c60152.netlify.app/",
   },
   {
-    id: 13,
+    id: 14,
     title: "Vidtube - Youtube Clone",
     description: "This is a Front-End of a Youtube look-alike clone, Vidtube.",
     image: "/images/projects/vidtube.png",
@@ -147,7 +158,7 @@ const projectsData = [
     previewUrl: "https://vidtube-youtube-clonee.netlify.app/",
   },
   {
-    id: 14,
+    id: 15,
     title: "Sid Tasky App",
     description:
       "This is a basic note-taking app, where you can even have an image of notes represented.",
@@ -158,7 +169,7 @@ const projectsData = [
     previewUrl: "https://sid-tasky-app.netlify.app/",
   },
   {
-    id: 15,
+    id: 16,
     title: "Maruo Plant and Machinery",
     description:
       "This is a Front-End of a company which deals with construction vehicles and equipment, Maruo Plant and Machinery.",
@@ -169,7 +180,7 @@ const projectsData = [
     previewUrl: "https://whimsical-begonia567hello7world.netlify.app/",
   },
   {
-    id: 16,
+    id: 17,
     title: "Thaba Nchu SDA Church",
     description:
       "This is my first ever website built. It's a website of Thaba Nchu Seventh-Day Adventist church.",
